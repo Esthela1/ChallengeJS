950b32e2e279688ff9547704ae5f4336f28cd050
Esthela1 <esthela_salas1212@hotmail.com>
2018-09-05 18:13:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var massMark, heightMark, massJohn, heightJohn;
+
+massMark = 100;
+heightMark = 1.70;
+massJohn = 120;
+heightJohn = 1.90;
+
+heightMark = heightMark* 2;
+heightJohn = heightJohn * 2;
+
+massMark / heightMark;
+massJohn / heightJohn;
+
+
+
+
+
